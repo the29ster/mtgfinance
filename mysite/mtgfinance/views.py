2b5d0a5e0de6c563fc11800bdb1829302b19ab42
Nathan Whitney <nathanwhitney29@gmail.com>
2025-03-03 16:31:02 -0800

    added charts to the price history pages

--- mysite/mtgfinance/views.py
+++ mysite/mtgfinance/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .utils import fetch_card_data
 from .models import CardPriceHistory
+import json
 
 # Create your views here.
 
@@ -15,6 +16,20 @@ def homepage(request):
     return render(request, "index.html", {"cards": cards_data})
 
 def card_price_history(request, scryfall_id):
-    price_history = CardPriceHistory.objects.filter(card_name=scryfall_id).order_by('date')
+    price_entries = CardPriceHistory.objects.filter(card_name=scryfall_id).order_by("date")
+
+    # Extract dates and prices
+    dates = [entry.date.strftime("%Y-%m-%d") for entry in price_entries]
+    prices = [float(entry.price) for entry in price_entries]
+
+    dates_json = json.dumps(dates)
+    prices_json = json.dumps(prices)
+
     card_name = request.GET.get("name", "Unknown Card")
-    return render(request, "card_price_history.html", {"scryfall_id": scryfall_id, "price_history": price_history, "card_name": card_name})
+
+    return render(request, "card_price_history.html", {
+        "scryfall_id": scryfall_id,
+        "card_name": card_name,
+        "dates": dates_json,
+        "prices": prices_json
+    })
